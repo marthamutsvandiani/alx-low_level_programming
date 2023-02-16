fcbef36d4fce170fa3f6ac624de887e6a0f25ac4
marthamutsvandiani <marthakuts@gmail.com>
2023-02-16 11:18:11 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /**
  * main-Entry Point
- * Return: 0
+ *
+ * Return: Always (0) Success
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+	return (0);
+
 }
+	
+    
