8dca3bdb4590550e0974482fd923592d3a954025
marthamutsvandiani <marthakuts@gmail.com>
2023-02-21 06:20:35 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include ("main.h")
+
+/**
+ * print_alphabet-Printing alphabet in lower case
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+
+char letter;
+for (letter = 'a'; letter <= 'z';letter++)
+_putchar(letter);
+_putchar('\n');
+return (0);
+
+}
