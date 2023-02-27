6ccfd50cb13695d709c0c2aa00df969d0dfd3ce1
marthamutsvandiani <marthakuts@gmail.com>
2023-02-27 09:31:59 -0800

    pointers and arrays

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * positive_or_negative - assign a random number
+ * @i : integer
+ * if the number is greater than 0: is positive, if the number is 0: is zero
+ * if the number is less than 0: is negative, followed by a new line
+ * Return: 0
  */
-int main(void)
+
+void positive_or_negative(int i)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (n < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+	return (0);
 }
