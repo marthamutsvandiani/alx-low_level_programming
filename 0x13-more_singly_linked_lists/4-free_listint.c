f32c9508662c22c92f4ff5229db1ad506526d272
marthamutsvandiani <marthakuts@gmail.com>
2023-04-03 13:46:56 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,7 +4,7 @@
 
 /**
  * Entry-a function that fees a listint_t list
- * @current -the temporary listing to be freed
+ * @temporary -the temporary listing to be freed
  * @head -the start of the node
  * @listint_t - represent a singly linked list of integers
  * @next -represents the next node
@@ -13,13 +13,13 @@
 
 void free_listint(listint_t *head);
 {
-	listint_t *current;
+	listint_t *temporary;
 
 	while (head)
 	{
-		current = head;
+		temorary = head;
 		head = head->next;
-		free(current);
+		free(temporary);
 	}
 	free(head);
 
