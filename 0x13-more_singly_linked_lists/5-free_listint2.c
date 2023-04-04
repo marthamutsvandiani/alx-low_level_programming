def30db2b70eb17a8b41c44eb227f862069d4680
marthamutsvandiani <marthakuts@gmail.com>
2023-04-04 11:15:50 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,20 +14,20 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temporary;
 
-	if (head)
-	{
-		while (*head)
+	if (head == NULL)
+{
+		return;
+}
+
+		while (*head != NULL)
+
 	{
 		temporary = (*head);
 		*head = (*head)->next;
 		free(temporary);
 	}
-		else
-	{
-		return;
-	}
-		free(*head);
-		head = 0;
+
+		*head = NULL;
 }
 
 
